e6a05c4c7579c17511d6a277612c802850bfb270
H82M19\Student <Student>
2025-04-02 19:42:36 +0700

    add tuan7, set grid layout

---
+++ tuan7/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div id='container'>
+      <div id="side-bar">
+        side bar
+      </div>
+
+      <div id="main">
+        <div id="header">
+          header
+        </div>
+
+        <div id="slider">
+          slider
+        </div>
+
+        <div id="content">
+          content
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
